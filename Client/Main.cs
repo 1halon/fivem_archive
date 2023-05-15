aea7bd2ffe921ada33e24e2a1b4ef6541ad7629e
halondevelopment <45399503+halondevelopment@users.noreply.github.com>
2023-05-15 17:23:55 +0300

    Update

---
+++ Client/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace RealifevTemplate.Client
+{
+    public class Main : BaseScript
+    {
+        public Main()
+        {
+            Debug.WriteLine("Hi from RealifevTemplate.Client!");
+        }
+
+        [Tick]
+        public Task OnTick()
+        {
+            DrawRect(0.5f, 0.5f, 0.5f, 0.5f, 255, 255, 255, 150);
+
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ Server/Main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+
+namespace RealifevTemplate.Server
+{
+    public class Main : BaseScript
+    {
+        public Main()
+        {
+            Debug.WriteLine("Hi from RealifevTemplate.Server!");
+        }
+
+        [Command("hello_server")]
+        public void HelloServer()
+        {
+            Debug.WriteLine("Sure, hello.");
+        }
+    }
+}
