1ee9412907bf924b6657665ed9f983dd6c470169
halondevelopment <45399503+halondevelopment@users.noreply.github.com>
2023-06-01 19:59:46 +0300

    Update

--- csharp/src/Client/Main.cs
+++ csharp/src/Client/Main.cs
@@ -1,26 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using CitizenFX.Core;
 using static CitizenFX.Core.Native.API;
 
+using SharpConfig;
+
 namespace RealifevTemplate.Client
 {
+    using static Shared.Config;
     public class Main : BaseScript
     {
         public Main()
         {
-            Debug.WriteLine("Hi from RealifevTemplate.Client!");
-        }
+            HandleConfiguration(new Action<Section, Configuration>((section, config) =>
+            {
 
-        [Tick]
-        public Task OnTick()
-        {
-            DrawRect(0.5f, 0.5f, 0.5f, 0.5f, 255, 255, 255, 150);
-
-            return Task.FromResult(0);
+            }));
         }
     }
 }
--- csharp/src/Server/Main.cs
+++ csharp/src/Server/Main.cs
@@ -1,37 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
 using SharpConfig;
-using System.Threading;
 
 namespace RealifevTemplate.Server
 {
-    using static Shared.Main;
+    using static Shared.Config;
     public class Main : BaseScript
     {
         public Main()
         {
-            Debug.WriteLine("Hi from RealifevTemplate.Server!");
-
-            HandleConfig(new Action<Section, Configuration>((section, config) =>
+            HandleConfiguration(new Action<Section, Configuration>((section, config) =>
             {
-                Debug.WriteLine("HandleConfig");
+                
             }));
-        }
-
-        [Command("hello_server")]
-        public void HelloServer()
-        {
-            Debug.WriteLine("Sure, hello.");
-        }
-        
-        [EventHandler("onResourceStart")]
-        public void TestEvent(string resourceName)
-        {
-            Debug.WriteLine($"{resourceName} start");
         }
     }
 }
