6e4838f45b45c62ad3d82515d0ea54e3ae0270b0
halondevelopment <45399503+halondevelopment@users.noreply.github.com>
2023-06-01 11:29:36 +0300

    Update

--- csharp/src/Server/Main.cs
+++ csharp/src/Server/Main.cs
@@ -4,21 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CitizenFX.Core;
+using SharpConfig;
+using System.Threading;
 
 namespace RealifevTemplate.Server
 {
+    using static Shared.Main;
     public class Main : BaseScript
     {
         public Main()
         {
             Debug.WriteLine("Hi from RealifevTemplate.Server!");
 
+            HandleConfig(new Action<Section, Configuration>((section, config) =>
+            {
+                Debug.WriteLine("HandleConfig");
+            }));
         }
 
         [Command("hello_server")]
         public void HelloServer()
         {
             Debug.WriteLine("Sure, hello.");
+        }
+        
+        [EventHandler("onResourceStart")]
+        public void TestEvent(string resourceName)
+        {
+            Debug.WriteLine($"{resourceName} start");
         }
     }
 }
--- csharp/src/Shared/Main.cs
+++ csharp/src/Shared/Main.cs
@@ -12,20 +12,27 @@ namespace RealifevTemplate.Shared
     public class Main : BaseScript
     {
         public static Configuration Config { get; private set; }
+        public static string ResourceName { get; private set; }
+
+
+        public static void HandleConfig(Action<Section, Configuration> callback)
+        {
+            if (Config == null) throw new NotImplementedException("Config is not implemented");
+
+            callback.Invoke(Config[ResourceName], Config);
+        }
 
         public Main()
         {
             Debug.WriteLine("Hi from RealifevTemplate.Shared!");
+            if (ResourceName == null) ResourceName = GetCurrentResourceName();
             if (Config == null) Config = ConfigReader();
         }
 
         private Configuration ConfigReader()
         {
-            string resourceName = GetCurrentResourceName();
-            string data = LoadResourceFile(resourceName, "config.ini");
-
-            var loaded = Configuration.LoadFromString(data);
-            if (!loaded.Contains(resourceName)) return null;
+            var loaded = Configuration.LoadFromString(LoadResourceFile(ResourceName, "config.ini"));
+            if (!loaded.Contains(ResourceName)) return null;
 
             return loaded;
         }
