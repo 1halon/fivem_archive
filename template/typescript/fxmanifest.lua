8005c99360e31aed327d7d636b909eedb830e590
halondevelopment <45399503+halondevelopment@users.noreply.github.com>
2023-08-11 02:59:00 +0300

    Update

---
+++ template/typescript/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+dependencies {
+	'yarn',
+	'webpack'
+}
+
+client_script 'dist/client/**.js'
+server_script 'dist/server/**.js'
+shared_script 'dist/shared/**.js'
